d6564bc3383553a9d3c99c1012ca9606faf05065
esztermazi <mazieszter@gmail.com>
2020-01-07 13:24:05 +0100

    dd FilepartReader, FileWordAnalyzer methods

--- src/FilePartReader.java
+++ src/FilePartReader.java
@@ -1,2 +1,54 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+
 public class FilePartReader {
+    private String filePath;
+    private Integer fromLine;
+    private Integer toLine;
+
+
+    public FilePartReader() {
+    }
+
+    public String getFilePath(){
+        return filePath;
+    }
+
+    public void setup(String filePath, Integer fromLine, Integer toLine) {
+        if (toLine < fromLine) {
+            throw new IllegalArgumentException("toLine is smaller than fromLine.");
+        }
+        if (fromLine < 1) {
+            throw new IllegalArgumentException("fromLine is smaller than 1.");
+        }
+        this.filePath = filePath;
+        this.fromLine = fromLine;
+        this.toLine = toLine;
+    }
+
+    public String read() throws IOException {
+        return new String(Files.readAllBytes(Paths.get(filePath)));
+    }
+
+    public String readLines() throws IOException {
+        /*
+         */
+        String wholeFileContent;
+        wholeFileContent = read();
+        String[] wholeLines = wholeFileContent.split("(?<=\n)");
+
+        int wholeLength = wholeLines.length;
+        if (wholeLength < fromLine) {
+            return "";
+        }
+        int endLine = Math.min(toLine, wholeLength);
+        final int length = endLine - fromLine + 1;
+
+        String[] partLines = new String[length];
+        System.arraycopy(wholeLines, fromLine - 1, partLines, 0, length);
+
+        return String.join("", partLines);
+    }
 }
--- src/FileWordAnalyzer.java
+++ src/FileWordAnalyzer.java
@@ -1,2 +1,49 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class FileWordAnalyzer {
+    private FilePartReader filePartReader;
+
+    public FileWordAnalyzer(FilePartReader filePartReader) {
+        this.filePartReader = filePartReader;
+    }
+
+    public ArrayList<String> getWordsOrderedAlphabetically() throws IOException {
+        ArrayList<String> fileContent = new ArrayList<>(Arrays.asList(this.filePartReader.readLines().split(",")));
+        Collections.sort(fileContent, String.CASE_INSENSITIVE_ORDER);
+        return fileContent;
+    }
+
+    public List<String> getWordsContainingSubstring(String subString) throws IOException {
+        List<String> wordsContainingSubstring = null;
+        ArrayList<String> fileContent = new ArrayList<>(Arrays.asList(this.filePartReader.readLines().split(",")));
+        for (String element : fileContent) {
+            if (element.contains(subString)) {
+                wordsContainingSubstring.add(element);
+            }
+        }
+        return wordsContainingSubstring;
+    }
+
+    public List<String> getStringsWhichPalindromes() throws IOException {
+        List<String> wordsWhichArePalindromes = null;
+        ArrayList<String> fileContent = new ArrayList<>(Arrays.asList(this.filePartReader.readLines().split(",")));
+        for (String word : fileContent) {
+            int i1 = 0;
+            int i2 = word.length() - 1;
+            while (i2 > i1) {
+                if ((word.charAt(i1)) != (word.charAt(i2))) {
+                    break;
+                }
+                ++i1;
+                --i2;
+            }
+
+            wordsWhichArePalindromes.add(word);
+        }
+        return wordsWhichArePalindromes;
+    }
 }
